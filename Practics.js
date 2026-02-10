94dcef6ba8177b7426072a96ed8eed8d7ccf3d7f
Pranjali86 <jaiswalzais@gmail.com>
2026-02-10 19:12:53 +0530

    Practice file Uploaded

---
+++ Practics.js
@@ -0,0 +1,10 @@
+var workoutName = "String Manipulation";
+var difficultyLevel = 1;
+console.log("Starting Practice: " + workoutName);
+console.log("Level: " + difficultyLevel);
+if (difficultyLevel < 5) {
+    console.log("Status: Ready to learn the basics!");
+}
+else {
+    console.log("Status: Ready for a challenge!");
+}
---
+++ Practics.ts
@@ -0,0 +1,11 @@
+let workoutName: string = "String Manipulation"; 
+let difficultyLevel: number = 1;
+
+console.log("Starting Practice: " + workoutName); 
+console.log("Level: " + difficultyLevel);
+
+if (difficultyLevel < 5) { 
+    console.log("Status: Ready to learn the basics!"); 
+} else { 
+    console.log("Status: Ready for a challenge!"); 
+}
